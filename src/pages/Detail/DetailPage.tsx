3ee95f50f2b4adb6b38b86d86561046647a0bb99
johanse7 <johanrodriguez_14@hotmail.com>
2025-09-20 16:07:28 -0500

    feat: add vercel config

--- src/pages/Detail/DetailPage.tsx
+++ src/pages/Detail/DetailPage.tsx
@@ -8,6 +8,7 @@ import {
 
 import { getTypeClass, type PokemonTypeKey } from "@/helpers";
 import { Slider } from "@/shared/components";
+import layoutStyles from "@/shared/layouts/main-layout/MainLayout.module.css";
 import { Loader } from "@/shared/ui";
 import { Navigate, useParams } from "react-router";
 import styles from "./DetailPage.module.css";
@@ -34,25 +35,30 @@ export const DetailPage = () => {
   } = pokemonDetail;
 
   const firstType: PokemonTypeKey = (types?.[0] as PokemonTypeKey) ?? "steel";
+  const backgroundClass = getTypeClass(firstType);
 
   return (
-    <div className={`${styles.containerDetail} ${getTypeClass(firstType)}`}>
-      <div className={`${styles.wrapper} container`}>
+    <div
+      className={`${layoutStyles.layout} ${styles.containerDetail} ${backgroundClass}`}
+    >
+      <div className={`${layoutStyles.centeredContainer}`}>
         <HeaderDetails pokemonId={pokemonId} title={name} />
-        <section className={styles.detailInfo}>
-          <div className={styles.sliderPokemonContent}>
-            <Slider images={images} className={styles.sliderPokemon} />
-          </div>
-          <PokemonTypeList types={types as PokemonTypeKey[]} />
-          <AboutPokemon
-            type={firstType}
-            weight={weight}
-            height={height}
-            description={description}
-            moves={moves}
-          />
-          <BaseStats type={firstType} stats={stats} />
-        </section>
+        <div className={styles.wrapperDetail}>
+          <section className={styles.detailInfo}>
+            <div className={styles.sliderPokemonContent}>
+              <Slider images={images} className={styles.sliderPokemon} />
+            </div>
+            <PokemonTypeList types={types as PokemonTypeKey[]} />
+            <AboutPokemon
+              type={firstType}
+              weight={weight}
+              height={height}
+              description={description}
+              moves={moves}
+            />
+            <BaseStats type={firstType} stats={stats} />
+          </section>
+        </div>
       </div>
     </div>
   );
--- src/pages/Pokemon/PokemonPage.tsx
+++ src/pages/Pokemon/PokemonPage.tsx
@@ -1,11 +1,12 @@
 import { PokemonListContainer } from "@/features/pokemon/components";
+import layoutStyles from "@/shared/layouts/main-layout/MainLayout.module.css";
 import { FilterContainer } from "../../features/filter/components/FilterContainer/FilterContainer";
 import style from "./PokemonPage.module.css";
 
 export const PokemonPage = () => {
   return (
-    <div className={`${style.containerPokemonPage}`}>
-      <div className={`${style.wrapper} container`}>
+    <div className={`${layoutStyles.layout} ${style.containerPokemonPage}`}>
+      <div className={layoutStyles.centeredContainer}>
         <FilterContainer />
         <section className={style.listContainer}>
           <PokemonListContainer />
